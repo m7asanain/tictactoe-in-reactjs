50a606930bcfe798aa35f9cb3ae839f1e5746e3e
Mustafa Alkamali <m7asanain@gmail.com>
2023-10-10 21:31:37 +0300

    handle cell click

--- tic-tac-toe/app/components/cell.tsx
+++ tic-tac-toe/app/components/cell.tsx
@@ -1,8 +1,37 @@
+import { Dispatch, SetStateAction } from "react";
 
+type CellProps = {
+    id: number;
+    go: string;
+    setGo: Dispatch<SetStateAction<string>>;
+    cells: string[];
+    setCells: Dispatch<SetStateAction<string[]>>;
+}
 
-const Cell = () => {
+const Cell = ({ id, go, setGo, cells, setCells }: CellProps) => {
+
+    const handelClick = () => {
+        const notTaken = !cells[id];
+
+        if (notTaken) {
+            if (go === "circle") {
+                handelCellChange("circle");
+                setGo("cross")
+            } else if (go === "cross") {
+                handelCellChange("cross");
+                setGo("circle")
+            }
+        }
+    }
+
+    const handelCellChange = (cellToChange: string) => {
+        let copyCells = [...cells];
+        copyCells[id] = cellToChange;
+        setCells(copyCells);
+    }
+
     return (
-        <div className="square">
+        <div className="square" onClick={handelClick}>
             
         </div>
     );
--- tic-tac-toe/app/page.tsx
+++ tic-tac-toe/app/page.tsx
@@ -5,11 +5,16 @@ import Cell from './components/cell';
 
 export default function Home() {
   const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [go, setGo] = useState("circle");
+
+  console.log(cells);
+  
+
   return (
     <main className='container'>
       <div className='gameboard'>
         {cells.map((cell, index) => (
-          <Cell key={index} />
+          <Cell id={index} go={go} setGo={setGo} key={index} cells={cells} setCells={setCells} />
         ))}
       </div>
     </main>
